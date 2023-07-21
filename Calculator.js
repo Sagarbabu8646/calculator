78a94f88f17511b3bfcd60aa010e18bfab73241a
Sagarbabu8646 <139497625+Sagarbabu8646@users.noreply.github.com>
2023-07-21 22:12:28 +0530

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,10 @@
+var clickS=new Audio()
+clickS.src="./Click.mp3"
+function clickSound(){
+    clickS.play();
+}
+var click=new Audio()
+click.src="./WaterDrop.mp3"
+function clickSound1(){
+    click.play();
+}
